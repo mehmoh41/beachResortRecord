1b35bd1b2a2d82477c816f74834441c0b6dbb79a
mehdi mohammadi <moh.meh41@gmail.com>
2020-06-24 13:51:43 +0500

    server files seperated

--- src/contentful.js
+++ src/contentful.js
@@ -1,8 +1,8 @@
 import {createClient} from 'contentful';
 export default createClient({
-    space: 'v2o6socqtxnw',
+    space: process.env.REACT_APP_API_SPACE,
 
     // 
-    accessToken:'BKo3ZixUrL-nap8nBmIBK4RVf6NDR60dOXqSmszr6H4'
-    // process.env.REACT_APP_ACCESS_TOKEN
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    //'BKo3ZixUrL-nap8nBmIBK4RVf6NDR60dOXqSmszr6H4'
 })
